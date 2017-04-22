dc2ae86b699cbf8be666da3da5bb19a3144608ec
Mathias Buus <mathiasbuus@gmail.com>
2017-04-22 13:46:31 +0200

    make example more verbose

--- example/client.js
+++ example/client.js
@@ -2,4 +2,12 @@ var net = require('../')()
 
 var socket = net.connect(process.argv[2] || 'example')
 
+socket.on('peer', function (peer) {
+  console.log('(trying to connect to %s:%d)', peer.host, peer.port)
+})
+
+socket.on('connect', function () {
+  console.log('(connected)')
+})
+
 process.stdin.pipe(socket).pipe(process.stdout)
